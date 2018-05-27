26536c5f9ce99937bf4448fcc410288840460ec0
jsolanes14 <jordisolanes14@gmail.com>
2018-05-28 00:50:59 +0200

    Removing ignored files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jsolanes14/djangostack-1.11.12-0/apps/django/bin/Practica1/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
